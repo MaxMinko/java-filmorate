4873e9ea922fe9641bad8a5868faff8060652897
Max Minko <minko.maks2015@yandex.ru>
2022-10-15 10:33:54 +0300

    исправлены замечания.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 @EqualsAndHashCode
 public class Film {
     private int id;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 @EqualsAndHashCode
 public class User {
     private int id;
@@ -13,4 +15,6 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+
+
 }
