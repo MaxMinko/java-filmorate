4a5a11664dab26de6432268bc822ea5d8440ff9f
Max Minko <minko.maks2015@yandex.ru>
2023-01-04 21:10:03 +0300

    add-database10

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
+INSERT INTO genres (GENRE_NAME)
+VALUES ('Комедия'), ('Драма'),('Мультфильм'),
+       ('Триллер'),
+       ('Документальный'),
+       ('Боевик');
 
+INSERT INTO MPA_NAME (NAME_MPA)
+VALUES ('G'), ('PG'),('PG-13'),
+       ('R'),
+       ('NC-17');
 
 
