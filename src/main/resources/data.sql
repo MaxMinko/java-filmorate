91a57612881dfdfa65d53df97ec680fd27e2b877
Max Minko <minko.maks2015@yandex.ru>
2023-01-04 21:06:59 +0300

    add-database9

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,3 @@
 
-INSERT INTO genres (GENRE_NAME)
-VALUES ('Комедия'), ('Драма'),('Мультфильм'),
- ('Триллер'),
- ('Документальный'),
-('Боевик');
-
-INSERT INTO MPA_NAME (NAME_MPA)
-VALUES ('G'), ('PG'),('PG-13'),
-       ('R'),
-       ('NC-17');
 
 
