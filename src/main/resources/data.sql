72bd5e2419f960bb4b6805c3b763af090e33cf43
Max Minko <minko.maks2015@yandex.ru>
2023-01-05 14:00:11 +0300

    add-database12

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,21 @@
-INSERT INTO genres (GENRE_NAME)
-VALUES ('Комедия'), ('Драма'),('Мультфильм'),
-       ('Триллер'),
-       ('Документальный'),
-       ('Боевик');
+DELETE
+from GENRES;
+INSERT INTO genres
+VALUES (1, 'Комедия'),
+       (2, 'Драма'),
+       (3, 'Мультфильм'),
+       (4, 'Триллер'),
+       (5, 'Документальный'),
+       (6, 'Боевик');
 
-INSERT INTO MPA_NAME (NAME_MPA)
-VALUES ('G'), ('PG'),('PG-13'),
-       ('R'),
-       ('NC-17');
+DELETE
+FROM MPA_NAME;
+INSERT INTO MPA_NAME
+VALUES ('G', 1),
+       ('PG', 2),
+       ('PG-13', 3),
+       ('R', 4),
+       ('NC-17', 5);
+
+
+
